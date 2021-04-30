1a2b10db038084d42f197fd57437ba5f354f2359
Pavani Tadepalli <67512847+Pavani123429@users.noreply.github.com>
2021-04-30 19:09:07 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -28,13 +28,13 @@ void InitLED(){
 
 //Set B1 as INPUT and initialize it to output HIGH
   
-    DDRB&=~(1<<PB1);
-    PORTB|=(1<<PB1);
+    DDRB&=~(1<<PB2);
+    PORTB|=(1<<PB2);
 
 //Set B2 as INPUT and initialize it to output HIGH
     
-    DDRB&=~(1<<PB2);
-    PORTB|=(1<<PB2);
+    DDRB&=~(1<<PB3);
+    PORTB|=(1<<PB3);
     }
    
         void ledon(void)
